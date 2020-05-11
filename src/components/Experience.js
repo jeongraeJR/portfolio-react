013ffc99c9ed57d7d5616b22607ee63d12c9e188
jeongraeJR <hwal92@naver.com>
2020-05-11 22:48:22 +0900

    remove cardContainer

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Container, Card, CardHeader, CardBody, CardTitle, CardText } from 'reactstrap';
-import {TitleContainer, ContentContainer} from './customStyle'
+import { Container } from 'reactstrap';
+import { TitleContainer, ContentContainer, SubTitleContainer } from './customStyle';
 
 const cardTab = {
-    'margin-top':'30px'
-}
+    'margin-top': '30px',
+};
 export default function Experience() {
     return (
         <div>
@@ -14,39 +14,30 @@ export default function Experience() {
                     <hr></hr>
                 </TitleContainer>
 
+                <SubTitleContainer>
+                    <h4> 가나네트웍스 (2019.06 ~ 2020.03) - 10개월</h4>
+                </SubTitleContainer>
                 <ContentContainer>
-                    <Card>
-                        <CardHeader tag="h4"> <b>가나네트웍스 (2019.06 ~ 2020.03)</b> - 10개월</CardHeader>
-                        <CardBody>
-                            <CardTitle>백엔드 개발자</CardTitle>
-                            <CardText>
-                                <ul>
-                                    <li>자사 서비스 REST API 개발</li>
-                                    <li>AWS인프라를 활용한 환경 구성</li>
-                                    <li>DB설계 참여</li>
-                                    <li>퇴사 사유 : 경영 악화</li>
-                                </ul>
-                            </CardText>
-
-                        </CardBody>
-                    </Card>
-
-                    <Card style={cardTab}>
-                        <CardHeader tag="h4">지에스소프트 (2018.01 ~ 2019.05) - 1년 4개월</CardHeader>
-                        <CardBody>
-                            <CardTitle>웹개발자</CardTitle>
-                            <CardText> 
-                                <ul>
-                                    <li>자사 CRM및 자동이체관리 프로그램 개발</li>
-                                    <li>프로그램 이용관련 고객응대</li>
-                                    <li>DB설계 참여</li>
-                                    <li>퇴사 사유 : 근무 조건</li>
-                                </ul>
-                            </CardText>
-                        </CardBody>
-                    </Card>
+                    <ul>
+                        <li>자사 서비스 REST API 개발</li>
+                        <li>AWS인프라를 활용한 서버 환경 구성</li>
+                        <li>관계형 DB설계 참여</li>
+                        <li>배포 자동화 설정</li>
+                        <li>퇴사 사유 : 경영 악화</li>
+                    </ul>
                 </ContentContainer>
 
+                <SubTitleContainer>
+                    <h4> 지에스소프트 (2018.01 ~ 2019.05) - 1년 4개월</h4>
+                </SubTitleContainer>
+                <ContentContainer>
+                    <ul>
+                        <li>자사 CRM및 자동이체관리 프로그램 개발</li>
+                        <li>프로그램 이용관련 고객응대</li>
+                        <li>관계형 DB설계 참여</li>
+                        <li>퇴사 사유 : 근무 조건</li>
+                    </ul>
+                </ContentContainer>
             </Container>
         </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 const HeaderContainer = styled.div`
     background-color: #000;
-    height: 200px;
+    height: 70px;
 `;
 export default function Header() {
     return <HeaderContainer></HeaderContainer>;
