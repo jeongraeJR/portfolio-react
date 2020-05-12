60520f596fbdd41bf4251a849eae74412df97401
jeongraeJR <hwal92@naver.com>
2020-05-12 15:43:18 +0900

    change header to nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    Nav,
+    NavbarBrand
+  } from 'reactstrap';
 import styled from 'styled-components';
-const HeaderContainer = styled.div`
+/* const HeaderContainer = styled.div`
     background-color: #000;
     height: 70px;
-`;
+`; */
 export default function Header() {
-    return <HeaderContainer></HeaderContainer>;
+    //return <HeaderContainer></HeaderContainer>;
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+          <Navbar color="light" light expand="md">
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="mx-auto" navbar>
+                <NavbarBrand href="/">About</NavbarBrand>
+                <NavbarBrand href="/">Skills</NavbarBrand>
+                <NavbarBrand href="/">Experience</NavbarBrand>
+                <NavbarBrand href="/">Projects</NavbarBrand>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      );
 }
