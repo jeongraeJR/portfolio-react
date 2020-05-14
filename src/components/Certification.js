1e5f9218deff1d5157cd31a9d17fc403cfc57d13
jeongraeJR <hwal92@naver.com>
2020-05-14 16:43:04 +0900

    자격증 설명 추가

--- src/components/Certification.js
+++ src/components/Certification.js
@@ -17,10 +17,20 @@ export default function Certification() {
                
                 <ContentContainer>
                     <h5><FontAwesomeIcon icon={faPencilAlt}></FontAwesomeIcon>&nbsp;&nbsp;정보처리기사 - 2018.08</h5>
+                    <ul>
+                        <li>취득일 : 2018.08.17</li>
+                        <li>저격번호 : 18202050850W</li>
+                        <li>기관 : 한국산업인력공단</li>
+                    </ul>
                 </ContentContainer>
 
                 <ContentContainer>
                     <h5><FontAwesomeIcon icon={faDatabase}></FontAwesomeIcon>&nbsp;&nbsp;국가공인 SQL개발자 (SQLD) - 2019.12</h5>
+                    <ul>
+                        <li>취득일 : 2019.12.31</li>
+                        <li>저격번호 : SQLD-0352180</li>
+                        <li>기관 : 한국데이터산업진흥원</li>
+                    </ul>
                 </ContentContainer>
             </Container>
         </div>
